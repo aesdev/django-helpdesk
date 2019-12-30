cf3e9ae35be1d278f6be33413b2c4bc25eddf1fc
Dilip Dwarak <dilip@altestore.com>
2019-12-30 16:26:50 -0500

    MOre fixes

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -575,7 +575,7 @@ class Ticket(models.Model):
         verbose_name_plural = _('Tickets')
 
     def __str__(self):
-        return ("%s %s", unicode(self.id).encode('utf-8'), unicode(self.title).encode('utf-8'))
+        return ("%s %s", str(unicode(self.id).encode('utf-8')), str(unicode(self.title).encode('utf-8')))
         # return '%s %s' % (self.id.encode('utf8'), self.title.encode('utf8'))
 
     def get_absolute_url(self):
